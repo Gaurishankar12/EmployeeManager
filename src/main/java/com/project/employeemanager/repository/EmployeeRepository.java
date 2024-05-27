21e036c6d554d806afd20db1c6ac8c62f387c8f0
gaurishankar chille <gaurishankarchille@gmail.com>
2024-05-27 11:35:24 +0530

    Repository

---
+++ src/main/java/com/project/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.project.employeemanager.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.employeemanager.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+		public void deleteEmployeeById(long id);
+		public Optional<Employee> findEmployeeById(Long id);
+}
